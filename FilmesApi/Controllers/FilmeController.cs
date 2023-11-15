0f0414f026e7c3d27711207c7f642b7332868b78
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-11-15 20:28:01 -0300

    Inicio dos estudos de api em c#

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController
+{
+    private List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void adcionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
